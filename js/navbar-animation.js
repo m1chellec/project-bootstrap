0eddab205e90adb3bc1a4a5ad5c9da51f5ae8aad
Michelle Souza <michelle.michellecosta@gmail.com>
2019-11-15 22:55:05 -0300

    projeto bootstrap

---
+++ js/navbar-animation.js
@@ -0,0 +1,11 @@
+$('#collapse-navbar').on('show.bs.collapse', function(){
+	$('.topcasafina-banner').css('transform', 'translate(-50%, 10%');
+
+
+});
+
+$('#collapse-navbar').on('hide.bs.collapse', function(){
+	$('.topcasafina-banner').css('transform', 'translate(-50%, -50%');
+
+
+});
